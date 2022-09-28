1507377e74d0a9d11c4e12d1858308798c0b33c1
julianBrandon <p0lterg3istt@gmail.com>
2022-09-28 12:01:17 +0200

    Add handlebar and data for three pizzas

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const app = express();
+
+// Make everything inside of public/ available
+app.use(express.static('public'));
+
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+// app.get(path, callback);
+
+app.get("/", (req, res, next) => {
+    console.log("this is the homepage");
+    res.sendFile(__dirname + '/views/index.html');
+
+});
+
+app.get("/contact", (req, res, next) => {
+    console.log("this is the contact page");
+    res.sendFile(__dirname + '/views/contact-page.html');
+
+});
+
+
+app.get("/pizzas/margherita", (req, res, next) => {
+    console.log("this is the Margherita page");
+    // res.send("");
+    // res.sendFile(__dirname + '/views/pizza-margherita.html');
+    // res.render(path (, data));
+
+    const data = {
+        title: "Pizza Margherita",
+        price: 8,
+        imgFile: "pizza-margherita.webp",
+        ingredients: ["mozzarella", "tomato sauce", "basilicum"]
+    }
+
+    res.render("pizza-page", data);
+
+});
+
+
+app.get("/pizzas/carbonara", (req, res, next) => {
+    console.log("this is the Carbonara page");
+    const data = {
+        title: "Pizza Carbonara",
+        price: 9,
+        imgFile: "pizza-carbonara.jpg",
+        ingredients: ["ham", "mozzarella", "tomato sauce", "basilicum",]
+    }
+
+    res.render("pizza-page", data);
+
+});
+
+
+app.get("/pizzas/funghi", (req, res, next) => {
+    console.log("this is the Funghi page");
+    const data = {
+        title: "Pizza Funghi",
+        price: undefined,
+        imgFile: "pizza-funghi.jpg",
+        ingredients: ["funghi", "mozzarella", "tomato", "basilicum"]
+    
+    }
+
+    res.render("pizza-page", data);
+
+});
+
+
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
+
+
